b191143a2b7e914b1a17825c2113dc463dd181fb
Daniel Guerrero <daniel.guerrero.94@gmail.com>
2018-11-02 19:44:59 -0300

    tests de dtb.h

---
+++ tests/test_dtb_deserializar.c
@@ -0,0 +1,155 @@
+#include <cspecs/cspec.h>
+#include "../Bibliotecas/dtb.h"
+
+context(test_dtb) {
+
+	describe("deserializa dtb") {
+
+		u_int32_t cant_archivos;
+		u_int32_t cant_lineas;
+		u_int32_t tamanio_direccion;
+		char *direccion_archivo = "/equipos/River.txt";
+		char *path_escriptorio = "/test.sh";
+		DTB *dtb_mock;
+		void *payload;
+		void *lista_archivos_mock;
+		ArchivoAbierto *archivo_abierto;
+
+		void create_mock()
+		{
+			dtb_mock = malloc(sizeof(DTB));
+			dtb_mock->gdtPID = 1;
+			dtb_mock->PC = 2;
+			dtb_mock->estado = 3;
+			dtb_mock->flagInicializacion = 0;
+			dtb_mock->cantidadLineas = 10;
+			dtb_mock->pathEscriptorio = path_escriptorio;
+		}
+
+		void mock_payload()
+		{
+			int tamanio = 0;
+			payload = DTB_serializar(dtb_mock, &tamanio);
+		}
+
+		void mock_archivo_abierto()
+		{
+			cant_lineas = 11;
+			tamanio_direccion = strlen(direccion_archivo);
+			size_t tamanio = sizeof(u_int32_t);
+			archivo_abierto = malloc(tamanio * 2 + tamanio_direccion);
+		}
+
+		void mock_lista_archivos()
+		{
+			cant_archivos = 1;
+
+			//Por ahora es un solo ArchivoAbierto
+			mock_archivo_abierto();
+			//Primero agrega un int que indica cuantos hay en la lista
+			//Depues el primer int es cantidad de lineas el segundo el len
+			//Y por ultimo el tamanio del string
+			size_t tamanio = sizeof(u_int32_t);
+			lista_archivos_mock = malloc(tamanio * 2 + tamanio_direccion);
+			int offset = 0;
+			memcpy(lista_archivos_mock, &cant_archivos, tamanio);
+			offset += tamanio;
+			memcpy(lista_archivos_mock + offset, &cant_lineas, tamanio);
+			offset += tamanio;
+            memcpy(lista_archivos_mock + offset, &tamanio_direccion, tamanio);
+			offset += tamanio;
+            memcpy(lista_archivos_mock + offset, direccion_archivo, tamanio_direccion);
+		}
+
+		void free_payload_mock()
+		{
+			free(payload);
+		}
+
+		void free_dtb_mock()
+		{
+			free(dtb_mock);
+		}
+
+		void free_lista_archivos_mock()
+		{
+			free(lista_archivos_mock);
+		}
+
+		before {
+			create_mock();
+			mock_payload();
+			mock_lista_archivos();
+		} end
+
+		after {
+			free_payload_mock();
+			free_dtb_mock();
+			free_lista_archivos_mock();
+		} end
+
+		/**
+		 * Nos sirve para poder mantener consistente el peso del dtb.
+		 * Solo va a fallar si cambia la definicion de los atributos staticos del dtb
+		 */
+		it("Puede crear un dtb") {
+			size_t tamanio = sizeof(*dtb_mock);
+			should_int(tamanio) be equal to(40);
+			should_int(dtb_mock->gdtPID) be equal to(1);
+		} end
+
+		it("Puede copiar datos staticos que vienen en payload") {
+			DTB *dtb = malloc(sizeof(DTB));
+			int offset = 0;
+			//Puedo consumir los 5 primeros int de una vez 
+			DTB_cargar_estaticos(dtb, payload, &offset);
+			should_int(dtb->gdtPID) be equal to(1);
+			should_int(dtb->PC) be equal to(2);
+			should_int(dtb->estado) be equal to(3);
+			should_int(dtb->flagInicializacion) be equal to(0);
+			should_int(dtb->cantidadLineas) be equal to(10);
+		} end
+
+		it("Puede copiar datos staticos y deja bien posicionado el offset") {
+			DTB *dtb = malloc(sizeof(DTB));
+			int offset = 0;
+			DTB_cargar_estaticos(dtb, payload, &offset);
+			should_int(offset) be equal to(20);
+		} end
+
+		it("Puede copiar el path del escriptorio") {
+			DTB *dtb = malloc(sizeof(DTB));
+			int offset = 20;
+			DTB_cargar_path_escriptorio(dtb, payload, &offset);
+			should_int(offset) be equal to(32);
+		} end
+
+		it("Puede crear lista a partir de payload") {
+			DTB *dtb = malloc(sizeof(DTB));
+			dtb->archivosAbiertos = list_create();
+
+			int desplazamiento = 0;
+			DTB_cargar_archivos_abiertos(dtb->archivosAbiertos, lista_archivos_mock, &desplazamiento);
+			should_int(list_size(dtb->archivosAbiertos)) be equal to(cant_archivos);
+
+			ArchivoAbierto *archivo = list_get(dtb->archivosAbiertos, 0);
+			should_int(archivo->cantLineas) be equal to(cant_lineas);
+			should_string(archivo->direccionArchivo) be equal to(direccion_archivo);
+		} end
+
+		it("Puede deserializar dtb") {
+			DTB *dtb = DTB_deserializar(payload);
+			should_int(dtb->cantidadLineas) be equal to(10);
+			should_string(dtb->pathEscriptorio) be equal to(path_escriptorio);
+			/*
+			should_int(list_size(dtb->archivosAbiertos)) be equal to(cant_archivos);
+			ArchivoAbierto *archivo = list_get(dtb->archivosAbiertos, 0);
+			should_int(archivo->cantLineas) be equal to(cant_lineas);
+			should_string(archivo->direccionArchivo) be equal to(direccion_archivo);
+			*/
+		} end
+
+
+	} end
+
+}
---
+++ tests/test_dtb_serializar.c
@@ -0,0 +1,103 @@
+#include <cspecs/cspec.h>
+#include "../Bibliotecas/dtb.h"
+
+context(test_dtb) {
+
+	describe("serializa dtb") {
+
+		DTB *un_dtb;
+		ArchivoAbierto *un_archivo;
+		ArchivoAbierto *otro_archivo;
+		t_list *una_lista;
+
+		before {
+			un_archivo = malloc(sizeof(ArchivoAbierto));
+			un_archivo->cantLineas = 10;
+			char *ejemplo = "ejemplo.txt";
+			un_archivo->direccionArchivo = malloc(strlen(ejemplo) + 1);
+			memcpy(un_archivo->direccionArchivo, ejemplo, strlen(ejemplo));
+
+			otro_archivo = malloc(sizeof(ArchivoAbierto));
+			otro_archivo->cantLineas = 11;
+			char *otro_ejemplo = "otro_ejemplo.txt";
+			otro_archivo->direccionArchivo = malloc(strlen(otro_ejemplo) + 1);
+			memcpy(otro_archivo->direccionArchivo, otro_ejemplo, strlen(otro_ejemplo));
+
+			una_lista = list_create();
+			list_add(una_lista, un_archivo);
+			list_add(una_lista, otro_archivo);
+
+			un_dtb = malloc(sizeof(DTB));
+			char *path = "path";
+			un_dtb->pathEscriptorio = malloc(strlen(path));
+			memcpy(un_dtb->pathEscriptorio, path, strlen(path));
+
+			un_dtb->archivosAbiertos = una_lista;
+		} end
+
+		after {
+			free(un_archivo);
+			free(otro_archivo);
+			list_destroy(una_lista);
+			free(un_dtb->pathEscriptorio);
+			free(un_dtb);
+		} end
+
+
+		it("Puede serializar archivo") {
+			int tamanio_serializado = 0;
+			void *serializado = DTB_serializar_archivo(un_archivo, &tamanio_serializado);
+			should_int(tamanio_serializado) be equal to(19);
+			
+			int desplazamiento = 0;
+        	ArchivoAbierto *deserializado = DTB_leer_struct_archivo(serializado, &desplazamiento);
+			should_int(deserializado->cantLineas) be equal to(10);
+			should_string(deserializado->direccionArchivo) be equal to("ejemplo.txt");
+		} end
+
+
+		it("Puede serializar lista") {
+			int tamanio_lista_serializada = 0;
+			void *lista_serializada = DTB_serializar_lista(una_lista, &tamanio_lista_serializada);
+			should_int(tamanio_lista_serializada) be equal to(43);
+
+			void *data = malloc(tamanio_lista_serializada + sizeof(u_int32_t));
+			int cant = 2;
+			memcpy(data, &cant, sizeof(u_int32_t));
+			memcpy(data + sizeof(u_int32_t), lista_serializada, tamanio_lista_serializada);
+			t_list *lista = list_create(); 
+			int desplazamiento = 0;
+			DTB_cargar_archivos_abiertos(lista, data, &desplazamiento);
+			should_int(list_size(lista)) be equal to(2);
+
+			ArchivoAbierto *a = list_get(lista, 0);
+			should_int(a->cantLineas) be equal to(10);
+			should_string(a->direccionArchivo) be equal to("ejemplo.txt");
+
+			ArchivoAbierto *b = list_get(lista, 1);
+			should_int(b->cantLineas) be equal to(11);
+			should_string(b->direccionArchivo) be equal to("otro_ejemplo.txt");
+		} end
+
+		it("Puede serializar dtb entero") {
+			int tamanio_dtb = 0;
+			void *dtb_serializado = DTB_serializar(un_dtb, &tamanio_dtb);
+			should_int(tamanio_dtb) be equal to(75);
+
+			DTB *dtb = DTB_deserializar(dtb_serializado);
+			should_string(dtb->pathEscriptorio) be equal to("path");
+			
+			ArchivoAbierto *a = list_get(dtb->archivosAbiertos, 0);
+			should_int(a->cantLineas) be equal to(10);
+			should_string(a->direccionArchivo) be equal to("ejemplo.txt");
+
+			ArchivoAbierto *b = list_get(dtb->archivosAbiertos, 1);
+			should_int(b->cantLineas) be equal to(11);
+			should_string(b->direccionArchivo) be equal to("otro_ejemplo.txt");
+
+		} end
+
+
+	} end
+
+}
